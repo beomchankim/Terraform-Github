8cd972ffa02ac3338235276adc4acca6dd1c37c1
beomchankim <78011531+beomchankim@users.noreply.github.com>
2021-08-05 15:02:31 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,14 +35,14 @@ resource "azurerm_subnet" "main" {
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
   
-  delegation {
-    name = "subnet-webapp"
+#   delegation {
+#     name = "subnet-webapp"
 
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
+#     service_delegation {
+#       name    = "Microsoft.Web/serverFarms"
+#       actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+#     }
+#   }
 }
 
 
