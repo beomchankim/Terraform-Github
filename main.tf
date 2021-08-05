824a39b522b11599ac55e95a6b08fd042ed199de
beomchankim <78011531+beomchankim@users.noreply.github.com>
2021-08-05 15:14:57 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,8 +66,8 @@ resource "azurerm_kubernetes_cluster" "main" {
     name           = "default"
     node_count     = 1
     vm_size        = "Standard_D2_v2"
-    network_vnet_id   = azurerm_virtual_network.main.id
-    network_subnet_id = azurerm_subnet.main.id
+    vnet_id        = azurerm_virtual_network.main.id
+    vnet_subnet_id = azurerm_subnet.main.id
   }
 
   service_principal {
